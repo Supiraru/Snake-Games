a12c45f822430b7e1ffd6b1de3c3d7e1d310f599
Supiraru <farhandewanta11@gmail.com>
2020-07-23 22:32:02 +0700

    LAST TRY

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+var isRendered = true;
+var length = 3;
+var lose = false;
+var condition = false;
+
+var food = {
+    x: 500,
+    y: 460,
+    w: 20,
+    h: 20,
+    val: false
+};
 
 //snake
 var snake = [];
 var tmp = 0;
-for(var i = 0; i < 5; i++){
-    snake[i] = {
-        w: 25,
-        h: 25,
-        x: canvas.width/2-35 + tmp,
-        y: canvas.height/2, 
-        dx: 25,
-        dy: 0
-    };
-    tmp = tmp - snake[i].w;
+var detWall = false;
+for(var i = 0; i < 200; i++){
+    if(i < 3){
+        snake[i] = {
+            w: 20,
+            h: 20,
+            x: canvas.width/2 + tmp,
+            y: canvas.height/2, 
+            dx: 20,
+            dy: 0
+        };
+        tmp = tmp - snake[i].w;
+    }
+    else{
+        snake[i] = {
+            w: 20,
+            h: 20,
+            x: -20,
+            y: -20, 
+            dx: 0,
+            dy: 0
+        };
+    }
 }
 
 function drawSnake(){
-    for(var j = 0; j < 5; j++){
+    for(var j = 0; j < length; j++){
         context.fillStyle = 'red';
         context.fillRect(snake[j].x, snake[j].y, snake[j].w, snake[j].h);
     }
@@ -28,22 +53,27 @@ function clear(){
 }
 
 function MovPos() {
-    var xPrev, yPrev, xNow, yNow ;
-    for(var i = 0; i < 5; i++){
-        if(i === 0){
-            xPrev = snake[i].x;
-            yPrev = snake[i].y;
-            snake[i].x += snake[i].dx;
-            snake[i].y += snake[i].dy;
-            
+    if(detWall === false){
+        if(isRendered === false){
+            isRendered = true;
         }
-        else{
-            xNow = snake[i].x;
-            yNow = snake[i].y;
-            snake[i].x = xPrev;
-            snake[i].y = yPrev;
-            xPrev = xNow;
-            yPrev = yNow;
+        var xPrev, yPrev, xNow, yNow ;
+        for(var i = 0; i < length; i++){
+            if(i === 0){
+                xPrev = snake[i].x;
+                yPrev = snake[i].y;
+                snake[i].x += snake[i].dx;
+                snake[i].y += snake[i].dy;
+                
+            }
+            else{
+                xNow = snake[i].x;
+                yNow = snake[i].y;
+                snake[i].x = xPrev;
+                snake[i].y = yPrev;
+                xPrev = xNow;
+                yPrev = yNow;
+            }
         }
     }
     detectWalls();
@@ -64,55 +94,121 @@ function MovPos() {
     if (snake[0].y + snake[0].h >= canvas.height) {
         snake[0].dy = 0;
     }  
+    if(snake[0].dx === 0 && snake[0].dy === 0){
+        detWall = true;
+    }
 }
+
+
+//keyDown
+function moveRight() {
+    snake[0].dx = 20;
+    snake[0].dy = 0;
+    detWall = false;
+}
+  
+function moveLeft() {
+    snake[0].dx = -20;
+    snake[0].dy = 0;
+    detWall = false;
+}
+
+function moveUp() {
+    snake[0].dx = 0;
+    snake[0].dy = -20;
+    detWall = false;
+}
+  
+function moveDown() {
+    snake[0].dx = 0;
+    snake[0].dy = 20;
+    detWall = false;
+}
+
+function keyDown(e) {
+    if(isRendered === true){
+        if (e.key === 'ArrowRight' || e.key === 'Right') {
+            if(snake[0].dx != -20 ){
+                moveRight();
+            }
+        } else if (e.key === 'ArrowLeft' || e.key === 'Left') {
+            if(snake[0].dx != 20){
+                moveLeft();
+            }
+        }
+        else if (e.key === 'ArrowUp' || e.key === 'Up') {
+            if(snake[0].dy != 20 ){
+                moveUp();
+            }
+        }
+        else if (e.key === 'ArrowDown' || e.key === 'Down') {
+            if(snake[0].dy != -20 ){
+                moveDown();
+            }
+        }
+        isRendered = false;
+    }
+}
+
+document.addEventListener('keydown', keyDown);
+
+
+//Food
+function drawFood(){
+    checkFood();
+    if(food.val === false){
+        context.fillStyle = 'blue';
+        context.fillRect(food.x, food.y, food.w, food.h);
+    }
+    else{
+        food.x = Math.floor(Math.random() * 50) * 20;
+        food.x = Math.floor(Math.random() * 30) * 20;
+        food.val = false;
+    }
+}
+
+function checkFood(){
+    if(snake[0].x === food.x && snake[0].y === food.y){
+        setTimeout(function(){food.val = true}, 80);
+        length++;
+    }
+}
+
+function detectBody(){
+    for(var i = 1; i < length; i++){
+        if(snake[0].x === snake[i].x && snake[0].y === snake[i].y){
+            lose = true;
+        }
+    }
+}
+
+function ScoreBoard(){
+    document.getElementById("score").innerHTML = "YOUR SCORE : " + length * 10;
+}
+
 
 function movement(){
     clear();
     drawSnake();
     MovPos();
+    drawFood();
+    detectBody();
+    ScoreBoard();
 }
 
-setInterval(movement, 1000);
-
-function moveRight() {
-    snake[0].dx = 25;
-    snake[0].dy = 0;
-}
-  
-function moveLeft() {
-    snake[0].dx = -25;
-    snake[0].dy = 0;
-}
-function moveUp() {
-    snake[0].dx = 0;
-    snake[0].dy = -25;
-}
-  
-function moveDown() {
-    snake[0].dx = 0;
-    snake[0].dy = 25;
-}
-
-function keyDown(e) {
-    if (e.key === 'ArrowRight' || e.key === 'Right') {
-        if(snake[0].dx != -25 ){
-            moveRight();
-        }
-    } else if (e.key === 'ArrowLeft' || e.key === 'Left') {
-        if(snake[0].dx != 25){
-            moveLeft();
-        }
+function reset(){
+    if(lose === false){
+        movement();
     }
-    else if (e.key === 'ArrowUp' || e.key === 'Up') {
-        if(snake[0].dy != 25 ){
-            moveUp();
-        }
-    }
-    else if (e.key === 'ArrowDown' || e.key === 'Down') {
-        if(snake[0].dy != -25 ){
-            moveDown();
-        }
+    else{
+        alert("YOU LOSE !");
+        condition = true;
     }
 }
 
-document.addEventListener('keydown', keyDown);
+if(condition == false){
+    setInterval(reset, 200);
+}
+else{
+    window.location.reload(true);
+}
